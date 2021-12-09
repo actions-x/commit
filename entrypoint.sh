2da697ea4b9fbc2daffcdcebfbd922c4c11cea6e
Mustafa Zengin <mzengin88@gmail.com>
2021-12-09 14:52:18 -0800

    rename working-dir to directory in the script

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ echo "  password $INPUT_TOKEN" >> "$HOME/.netrc"
 git config user.email "$INPUT_EMAIL"
 git config user.name "$INPUT_NAME"
 
-cd "$INPUT_WORKING-DIR" || exit 1
+cd "$INPUT_DIRECTORY" || exit 1
 
 # shellcheck disable=SC2086
 git add $INPUT_FILES -v
